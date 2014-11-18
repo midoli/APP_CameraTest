74616062555bcf6706ba1a95e08431d1cc7d1522
midoli <gary@midolitech.net>
2014-11-18 16:22:32 +0800

    u

--- app/view/Item/ItemPanel.js
+++ app/view/Item/ItemPanel.js
@@ -15,14 +15,27 @@
 					navigator.camera.getPicture(
 						function(imageURI){
 						 //alert(imageURI);
-						 
-						 var myObject, afile, size;
-						 myObject = new ActiveXObject("Scripting.FileSystemObject");
-						 afile = myObject.GetFile(imageURI)
-						 size = afile.Size;
-						 alert(size);
-						 //var xhr = new XMLHttpRequest();
-						 //xhr.open('POST', '/SP_Upload/DDRoomFileupload.ashx?FolderUrl=' + encodeURIComponent($("#html_folder").val()));
+						  var url=encodeURI("http://sps2013-dev/SP_Upload/DDRoomFileupload.ashx?FolderUrl=");
+						  //var username='your_user';
+						  //var password='your_pwd';
+
+
+						  var params = new Object();
+							params.your_param_name = "something";  //you can send additional info with the file
+
+							var options = new FileUploadOptions();
+							options.fileKey = "the_name_of_the_image_field"; //depends on the api
+							options.fileName = imageURI.substr(imageURI.lastIndexOf('/')+1);
+							options.mimeType = "image/jpeg";
+							options.params = params;
+							options.chunkedMode = true; //this is important to send both data and files
+
+							//var headers={'Authorization':"Basic " + Base64.encode(username + ":" + password)};
+							//options.headers = headers;
+
+							var ft = new FileTransfer();
+							ft.upload(imageURI, url, function(){alert("suc");}, function(){alert("err");}, options);
+
 						},
 						function(message){
 						 alert('Failed because: ' + message);
