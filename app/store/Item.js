f1ffdcdb3dde736f1b0fad1f18aab8e0a4c3f7e3
midoli <gary@midolitech.net>
2014-12-04 17:59:04 +0800

    1

--- app/store/Item.js
+++ app/store/Item.js
@@ -7,7 +7,7 @@
         model: 'CaremaTest.model.Item',
         proxy: {
             type: 'jsonp',
-            url: 'http://192.168.9.37/SP_Upload/DDRoomFile.ashx',
+            url: '',//'http://59.124.220.39:8181/SP_Upload/DDRoomFile.ashx',
             extraParams: {
                 FolderUrl: ''
             },
